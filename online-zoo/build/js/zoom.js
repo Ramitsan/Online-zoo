f82e64db17b639d9c50abdbff69e02d7575ec9b3
Ramitsan <ramitsan@gmail.com>
2021-05-01 01:25:40 +0300

    feat: add map drag&drop

--- online-zoo/build/js/zoom.js
+++ online-zoo/build/js/zoom.js
@@ -13,8 +13,8 @@
   const btnPlus = document.querySelector('.map__btn--plus');
   const mapContainerElement = document.querySelector('.map__container');
 
+  // зум
   const setImgScale = (scale) => {
-    // mapContainerElement.style.transform = 'scale(' + (scale / SCALE_START) + ')';
     mapContainerElement.style.transform = `scale(${scale / SCALE_START})`;
     currentScale = scale;
   };
@@ -35,4 +35,54 @@
   window.scaleIndicatorDefault = () => {
     setImgScale(SCALE_START);
   };
+
+
+  // переттаскивание
+  const headerElement = document.getElementById('header');
+  const footerElement = document.getElementById('footer');
+  let topIndent = 0;
+  let leftIndent = 0;
+
+  // let topIndent = mapContainerElement.style.top;
+  // let leftIndent = mapContainerElement.style.left;
+
+  const calculateCoords = (evt) => {
+    // let box = elem.getBoundingClientRect();
+    topIndent = evt.pageY + pageYOffset;
+    leftIndent = evt.pageX + pageXOffset;
+  };
+
+  const moveAt = (evt) => {
+    mapContainerElement.style.left = evt.pageX - leftIndent + 'px';
+    mapContainerElement.style.top = evt.pageY - topIndent + 'px';
+  };
+
+  const stopDrag = () => {
+    document.removeEventListener('mousemove', moveAt);
+    mapContainerElement.removeEventListener('mouseup', stopDrag);
+  };
+
+  // const resetMapMove = () => {
+  //   mapContainerElement.style.left = leftIndent;
+  //   mapContainerElement.style.top = topIndent;
+  // };
+
+  mapContainerElement.addEventListener('mousedown', (evt) => {
+
+    calculateCoords(evt);
+    moveAt(evt);
+    // resetMapMove();
+
+    document.addEventListener('mousemove', moveAt);
+    mapContainerElement.addEventListener('mouseup', stopDrag);
+  });
+
+
+  mapContainerElement.ondragstart = function() {
+    return false;
+  };
+
+  headerElement.addEventListener('mouseenter', stopDrag);
+  footerElement.addEventListener('mouseenter', stopDrag);
+
 })();
