e12491560a3d63db6ca1133532a5d38f802782d9
Ramitsan <ramitsan@gmail.com>
2021-06-16 01:07:21 +0300

    Updates

---
+++ js/map.js
@@ -0,0 +1,111 @@
+'use strict';
+
+(function() {
+
+  const SCALE_MIN = 100;
+  const SCALE_MAX = 200;
+  const SCALE_STEP = 25;
+  const SCALE_START = 100;
+
+  let currentScale = SCALE_START;
+
+  const btnMinus = document.querySelector('.map__btn--minus');
+  const btnPlus = document.querySelector('.map__btn--plus');
+  const mapContainerElement = document.querySelector('.map__container');
+
+  // зум
+  const setImgScale = (scale) => {
+    mapContainerElement.style.transform = `scale(${scale / SCALE_START})`;
+    currentScale = scale;
+  };
+
+  btnMinus.addEventListener('click', () => {
+    let nextScale = currentScale - SCALE_STEP;
+    if (nextScale >= SCALE_MIN) {
+      setImgScale(nextScale);
+    }
+  });
+  btnPlus.addEventListener('click', () => {
+    let nextScale = currentScale + SCALE_STEP;
+    if (nextScale <= SCALE_MAX) {
+      setImgScale(nextScale);
+    }
+  });
+
+  window.scaleIndicatorDefault = () => {
+    setImgScale(SCALE_START);
+  };
+
+
+  // переттаскивание
+  const headerElement = document.getElementById('header');
+  const footerElement = document.getElementById('footer');
+  let topIndent = 0;
+  let leftIndent = 0;
+
+  // let topIndent = mapContainerElement.style.top;
+  // let leftIndent = mapContainerElement.style.left;
+
+  const calculateCoords = (evt) => {
+    // let box = elem.getBoundingClientRect();
+    topIndent = evt.pageY + pageYOffset;
+    leftIndent = evt.pageX + pageXOffset;
+  };
+
+  const moveAt = (evt) => {
+    mapContainerElement.style.left = evt.pageX - leftIndent + 'px';
+    mapContainerElement.style.top = evt.pageY - topIndent + 'px';
+  };
+
+  const stopDrag = () => {
+    document.removeEventListener('mousemove', moveAt);
+    mapContainerElement.removeEventListener('mouseup', stopDrag);
+  };
+
+  // const resetMapMove = () => {
+  //   mapContainerElement.style.left = leftIndent;
+  //   mapContainerElement.style.top = topIndent;
+  // };
+
+  mapContainerElement.addEventListener('mousedown', (evt) => {
+
+    calculateCoords(evt);
+    moveAt(evt);
+
+    document.addEventListener('mousemove', moveAt);
+    mapContainerElement.addEventListener('mouseup', stopDrag);
+    // resetMapMove();
+  });
+
+
+  mapContainerElement.ondragstart = function() {
+    return false;
+  };
+
+  headerElement.addEventListener('mouseenter', stopDrag);
+  footerElement.addEventListener('mouseenter', stopDrag);
+
+  // открытие тултипа по клику на иконку животного
+  const mapElement = document.querySelector('.map');
+  const animalsIconsElements = document.querySelectorAll('.map__animals-item');
+
+  animalsIconsElements.forEach((icon) => {
+    icon.addEventListener('click', function() {
+      removeActiveIcon();
+      icon.classList.add('map__animals-item--active');
+    })
+  });
+
+  const removeActiveIcon = () => {
+    let activeIcon = document.querySelector('.map__animals-item--active');
+    if (activeIcon) {
+      activeIcon.classList.remove('map__animals-item--active');
+    }
+  };
+
+  mapElement.addEventListener('click', (evt) => {
+    if (evt.target !== animalsIconsElements) {
+      removeActiveIcon();
+    }
+  }, true);
+})();
---
+++ js/script.js
@@ -0,0 +1,113 @@
+'use strict';
+
+// var pageHeader = document.querySelector('.page-header');
+// var headerToggle = document.querySelector('.page-header__toggle');
+
+// pageHeader.classList.remove('page-header--nojs');
+
+// headerToggle.addEventListener('click', function () {
+//   if (pageHeader.classList.contains('page-header--closed')) {
+//     pageHeader.classList.remove('page-header--closed');
+//     pageHeader.classList.add('page-header--opened');
+//   } else {
+//     pageHeader.classList.add('page-header--closed');
+//     pageHeader.classList.remove('page-header--opened');
+//   }
+// });
+
+(function() {
+  // слайдер в блоке Pets
+  const petsListsElements = document.querySelectorAll('.pets__list');
+  const btnLeft = document.querySelector('.pets__button--left');
+  const btnRight = document.querySelector('.pets__button--right');
+  let currentItem = 0;
+  let isEnabled = true;
+
+  function changeCurrentItem(n) {
+    currentItem = (n + petsListsElements.length) % petsListsElements.length;
+  }
+
+  function hideItem(direction) {
+    isEnabled = false;
+    petsListsElements[currentItem].classList.add(direction);
+    petsListsElements[currentItem].addEventListener('animationend', function() {
+      this.classList.remove('pets__list--active', direction);
+    })
+  }
+
+  function showItem(direction) {
+    petsListsElements[currentItem].classList.add('pets__list--next', direction);
+    petsListsElements[currentItem].addEventListener('animationend', function() {
+      this.classList.remove('pets__list--next', direction);
+      this.classList.add('pets__list--active');
+      isEnabled = true;
+    })
+  }
+
+  function previosItem(n) {
+    hideItem('to-right');
+    changeCurrentItem(n - 1);
+    showItem('from-left');
+  }
+
+  function nextItem(n) {
+    hideItem('to-left');
+    changeCurrentItem(n + 1);
+    showItem('from-right');
+  }
+
+  btnLeft.addEventListener('click', function() {
+    if (isEnabled) {
+      previosItem(currentItem);
+    }
+  })
+
+  btnRight.addEventListener('click', function() {
+    if (isEnabled) {
+      nextItem(currentItem);
+    }
+  });
+
+  // слайдер в блоке Testimonials
+  const gap = 32;
+  const testimonialsCarousel = document.querySelector('.testimonials__carousel');
+  const testimonialsList = document.querySelector('.testimonials__list');
+  const testimonialsCountRange = document.querySelector('#testimonials-count');
+
+  let slideIndex = 0;
+  testimonialsCountRange.value = 0;
+
+  let width = testimonialsCarousel.offsetWidth;
+  let cardWidth = document.querySelector('.testimonial-card').offsetWidth;
+
+  window.addEventListener('resize', (e) => {
+    width = testimonialsCarousel.offsetWidth;
+    cardWidth = document.querySelector('.testimonial-card').offsetWidth;
+  });
+
+  const slideFunc = () => {
+    slideIndex += 1;
+    if (slideIndex >= 8) {
+      slideIndex = 0;
+    }
+
+    testimonialsList.scrollTo((cardWidth + gap) * slideIndex, 0);
+    testimonialsCountRange.value = slideIndex;
+  };
+
+  let autoSlideInterval = setInterval(slideFunc, 10000);
+  let autoSlideTimeout = null;
+
+  const delayAutoSliding = () => {
+    clearTimeout(autoSlideTimeout);
+    clearInterval(autoSlideInterval);
+    autoSlideInterval = null;
+
+    autoSlideTimeout = setTimeout(() => {
+      clearInterval(autoSlideInterval);
+      autoSlideInterval = setInterval(slideFunc, 10000);
+    }, 30000);
+  };
+
+  testimonialsList.addEventListener('click', delayAutoSliding);
+})();
